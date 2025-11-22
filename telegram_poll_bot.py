fa7723538997db6cf4988141195d2a4a7d62328a
Mario Esposito <48980637+marioexpo22@users.noreply.github.com>
2025-11-22 19:46:41 +0100

    Add files via upload
    inizio progetto

---
+++ telegram_poll_bot.py
@@ -0,0 +1,25 @@
+import os
+import logging
+from telegram.ext import ApplicationBuilder, CommandHandler
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
+
+async def start(update, context):
+    await update.message.reply_text("Bot attivo e funzionante su Render!")
+
+def main():
+    if not BOT_TOKEN:
+        print("ERRORE: Variabile BOT_TOKEN non impostata")
+        return
+
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(CommandHandler("start", start))
+
+    logger.info("Bot avviato.")
+    app.run_polling()
+
+if __name__ == "__main__":
+    main()
